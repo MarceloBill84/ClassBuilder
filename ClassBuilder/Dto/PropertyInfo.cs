e9462be2bd36a1a0d2c1dc70b77bc68c5350b41b
MarceloBill84 <marcelobill@msn.com>
2021-10-13 21:10:09 -0300

    Implementing BuilderFactory

---
+++ ClassBuilder/Dto/PropertyInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace ClassBuilder.Dto
+{
+	public class PropertyInfo
+	{
+		public string Type { get; }
+		public string Name { get; }
+
+		public PropertyInfo(string type, string name)
+		{
+			Type = type;
+			Name = name;
+		}
+	}
+}
--- ClassBuilder/Factory/BuilderFactory.cs
+++ ClassBuilder/Factory/BuilderFactory.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using ClassBuilder.Dto;
+using ClassBuilder.Extensions;
+using System.Collections.Generic;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace ClassBuilder.Factory
@@ -17,19 +20,76 @@ namespace ClassBuilder.Factory
 
 			content.Append("{\n");
 
-			var className = GetClassName(fileContent);
+			var originalClassName = GetOriginalClassName(fileContent);
+			var newClassName = string.Concat(originalClassName, "Builder");
 
-			content.AppendLine(string.Concat("\tpublic class ", className));
+			content.AppendLine(string.Concat("\tpublic class ", newClassName));
 
 			content.AppendLine("\t{");
 
-			//GetProperties
+			var properties = GetPropertiesInfo(fileContent);
+
+			GeneratePrivateVariables(content, properties);
+
+			GenerateBuilderConstructor(content, newClassName);
+
+			GenerateMethodsToSetValues(content, newClassName, properties);
+
+			GenerateMethodBuild(content, originalClassName, properties);
 
 			content.AppendLine("\t}");
 
 			content.AppendLine("}");
 
 			return content.ToString();
+		}
+
+		private static void GenerateMethodBuild(StringBuilder content, string originalClassName, IList<PropertyInfo> properties)
+		{
+			content.AppendLine($"\t\tpublic {originalClassName} Build()");
+			content.AppendLine("\t\t{");
+			content.AppendLine($"\t\t\treturn new {originalClassName}");
+			content.AppendLine("\t\t\t{");
+
+			for (int i = 0; i < properties.Count; i++)
+			{
+				var setValue = $"\t\t\t\t{properties[i].Name} = _{properties[i].Name.GetWordWithFirstLetterDown()}";
+				if (i + 1 != properties.Count)
+					setValue = string.Concat(setValue, ",");
+
+				content.AppendLine(setValue);
+			}
+
+			content.AppendLine("\t\t\t};");
+			content.AppendLine("\t\t}");
+		}
+
+		private static void GenerateBuilderConstructor(StringBuilder content, string newClassName)
+		{
+			content.AppendLine();
+			content.AppendLine($"\t\tpublic {newClassName}()" + " { }");
+			content.AppendLine();
+		}
+
+		private static void GenerateMethodsToSetValues(StringBuilder content, string className, IList<PropertyInfo> properties)
+		{
+			foreach (var item in properties)
+			{
+				content.AppendLine($"\t\tpublic {className} With{item.Name}({item.Type} {item.Name.GetWordWithFirstLetterDown()})");
+				content.AppendLine("\t\t{");
+				content.AppendLine($"\t\t\t_{item.Name.GetWordWithFirstLetterDown()} = {item.Name.GetWordWithFirstLetterDown()};");
+				content.AppendLine("\t\t\treturn this;");
+				content.AppendLine("\t\t}");
+				content.AppendLine();
+			}
+		}
+
+		private static void GeneratePrivateVariables(StringBuilder content, IList<PropertyInfo> properties)
+		{
+			foreach (var item in properties)
+			{
+				content.AppendLine($"\t\tprivate {item.Type} _{item.Name.GetWordWithFirstLetterDown()};");
+			}
 		}
 
 		private static string GetNameSpace(string fileContent)
@@ -42,11 +102,23 @@ namespace ClassBuilder.Factory
 			return fileContent.Substring(0, fileContent.IndexOf("namespace"));
 		}
 
-		private static string GetClassName(string fileContent)
+		private static string GetOriginalClassName(string fileContent)
 		{
-			var teste = Regex.Match(fileContent, @"\s+(class)\s+(?<Name>[^\s]+)");
+			var regex = Regex.Match(fileContent, @"\s+(class)\s+(?<Name>[^\s]+)");
 
-			return string.Concat(teste.Groups["Name"].Value, "Builder");
+			return regex.Groups["Name"].Value;
+		}
+
+		private static IList<PropertyInfo> GetPropertiesInfo(string fileContent)
+		{
+			var propertyes = new List<PropertyInfo>();
+
+			foreach (Match item in Regex.Matches(fileContent, @"(?>public)\s+(?!class)((static|readonly)\s)?(?<Type>[^\s\(]+)\s+(?<Name>[^\s\(]+)\s+(?!\()"))
+			{
+				propertyes.Add(new PropertyInfo(item.Groups["Type"].Value, item.Groups["Name"].Value));
+			}
+
+			return propertyes;
 		}
 	}
 }
